47ebc790b41ecde6170e644ee1b1c1f35d2fe124
akc <alicia15kc@gmail.com>
2018-09-07 10:18:51 -0400

    Write data to firebase

--- app/routes.py
+++ app/routes.py
@@ -14,6 +14,51 @@ def index():
     result = dbapp.get('/test', None)
     return result
 
+
+@app.route('/datastore')
+def data_store():
+    dbapp = firebase.FirebaseApplication('https://skim-7d0f7.firebaseio.com/', None)
+
+    fetched_data = urllib.request.urlopen(URL).read().decode('utf-8')
+    class_data = stripHTML(fetched_data)
+
+    for x in range(len(class_data)):
+        courses = class_data[x]
+
+        for s in range(len(courses)):
+            newline = courses[s].split("  ")
+
+            if newline[0].isalpha():
+                newline.remove(newline[0])
+
+            while '' in newline:
+                newline.remove('')
+
+            #fix waitlist issues
+            
+            classEntry = {"courseAndSection": newline[1], "name": newline[2], "maxEnrollment": newline[4], "currentEnrollment": newline[5], "waitlist": newline[7].split(" ")[:1], "semester": "spring", "year": "2018"}
+
+            dbapp.post('https://skim-7d0f7.firebaseio.com/', classEntry)
+
+
+    # courses = class_data[0]
+    # newline = courses[0].split("  ")
+    #
+    # if newline[0].isalpha():
+    #     newline.remove(newline[0])
+    #
+    # while '' in newline:
+    #     newline.remove('')
+    #
+    # classEntry = {"courseAndSection": newline[0], "name": newline[1], "maxEnrollment": newline[3], "currentEnrollment": newline[4], "waitlist": newline[6], "semester": "spring", "year": "2018"}
+    #
+    # dbapp.post('https://skim-7d0f7.firebaseio.com/', classEntry)
+
+    return "0"
+
+
+
+
 def stripHTML(data):
     split_data = [x.strip() for x in data.split('\n')]
     results = []
@@ -38,3 +83,20 @@ def data_fetch():
         s += '<br><br>'
         print('\n'.join(section))
     return s
+
+
+# def data_store():
+#     AccountingForBusiness = {"courseNumber": "210", "sectionNumber": "01", "name": "Accounting For Business", "maxEnrollment": "53", "currentEnrollment": "49", "waitlist": "0"} db.child("skim-7d0f7").push(AccountingForBusiness, user['idToken'])
+
+# dbapp = db.reference('https://skim-7d0f7.firebaseio.com/')
+# classes_dbapp = dbapp.child('classes')
+# classes_dbapp.set({
+#     'alanisawesome': {
+#         'date_of_birth': 'June 23, 1912',
+#         'full_name': 'Alan Turing'
+#     },
+#     'gracehop': {
+#         'date_of_birth': 'December 9, 1906',
+#         'full_name': 'Grace Hopper'
+#     }
+# })
