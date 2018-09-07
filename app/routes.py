44c305a52c6e518769c6e924636e8e57c599fb56
Rajat Khanna <khanna.rjt@gmail.com>
2018-09-06 21:24:25 -0400

    Able to scrape UAH url

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,21 @@
 from app import app
-
 from firebase import firebase
+import datetime
+import json
+import urllib.request
+
+URL = 'https://www.uah.edu/cgi-bin/schedule.pl?file=sprg2018.html&segment=ACC'
 
 @app.route('/')
+
 @app.route('/index')
 def index():
     dbapp = firebase.FirebaseApplication('https://skim-7d0f7.firebaseio.com/', None)
     result = dbapp.get('/test', None)
     return result
+
+@app.route('/data')
+def data_fetch():
+    url = urllib.request.urlopen(URL)
+    output = url.read()
+    return output
