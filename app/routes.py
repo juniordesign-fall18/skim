92ef941c93854f2f5eb2c3e5a00b08add1d1e1ab
Rajat Khanna <khanna.rjt@gmail.com>
2018-09-06 20:51:21 -0400

    Migrated code to python 3.4.9

--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,10 @@
 from app import app
 
 from firebase import firebase
-firebase = firebase.FirebaseApplication('https://skim-7d0f7.firebaseio.com/', None)
-result = firebase.get('/test', None)
-print result
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    dbapp = firebase.FirebaseApplication('https://skim-7d0f7.firebaseio.com/', None)
+    result = dbapp.get('/test', None)
+    return result
